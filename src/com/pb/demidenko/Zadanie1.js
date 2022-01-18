3be3ac2dc2ab464e4cb207678289a7ffd2adc163
Vadim-Demydenko <92310303+Vadim-Demydenko@users.noreply.github.com>
2022-01-18 09:48:32 +0200

    Update Zadanie1.js

--- src/com/pb/demidenko/Zadanie1.js
+++ src/com/pb/demidenko/Zadanie1.js
@@ -1,4 +1,9 @@
 var result;
- result = (a + b < 4) ?  true : false;
-
+var a = 3;
+var b = 4;
+result = (a + b < 4) ?  true : false;
+console.log(result);
+var a = 1;
+var b = 2;
+result = (a + b < 4) ?  true : false;
 console.log(result);
